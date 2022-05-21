52f351e558ad5c5882083e9b41e8399de93defd8
Masha <nasonkina@yandex.ru>
2022-05-21 21:09:32 +0300

    add index.js, rm test script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import 'bootstrap';
 import './styles/main.scss';
-import script from './js/script.js';
+import init from './js/init.js';
+import state from './js/state.js';
+import htmlElements from './js/htmlElements.js';
 
-script();
+import handleSubmit from './js/controllers.js';
+
+const watchedState = init(state);
+const { form } = htmlElements;
+form.addEventListener('submit', (/** @type {Event} */ e) => handleSubmit(e, watchedState));
--- src/js/script.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-  console.log(new Date().toLocaleDateString());
-};
