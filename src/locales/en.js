ede007221f862b1572c660041b25a375ab312a2e
Masha <nasonkina@yandex.ru>
2022-05-26 16:26:19 +0300

    add locales (ru, en)

---
+++ src/locales/en.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    validation: {
+      exists: 'RSS already exists',
+      invalid: 'RSS should be a valid URL',
+    },
+    network: {
+      success: 'RSS loaded successfully',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    validation: {
+      exists: 'RSS уже существует',
+      invalid: 'Ссылка должна быть валидным URL',
+    },
+    network: {
+      success: 'RSS успешно загружен',
+    },
+  },
+};
