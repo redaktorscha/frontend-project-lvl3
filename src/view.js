546624976cfbf8e35f56892c96cdfc642dfa6373
Masha <nasonkina@yandex.ru>
2022-07-25 04:02:45 +0300

    fix view

--- src/view.js
+++ src/view.js
@@ -1,4 +1,147 @@
 /**
+ * @param {Array<Object>} data
+ * @param {Object} htmlElements
+ */
+const handleFeeds = (data, htmlElements) => {
+  const { rssContainer, feedsList } = htmlElements;
+  rssContainer.classList.remove('d-none');
+  feedsList.innerHTML = '';
+  const feeds = [...data].map((feed) => {
+    const liElement = document.createElement('li');
+    liElement.classList.add('list-group-item', 'border-0');
+    const feedHeading = document.createElement('h3');
+    feedHeading.classList.add('h5', 'text-primary');
+    feedHeading.textContent = feed.title;
+    const feedDescription = document.createElement('p');
+    feedDescription.classList.add('text-small');
+    feedDescription.textContent = feed.description;
+    liElement.append(feedHeading, feedDescription);
+    return liElement;
+  });
+  feedsList.append(...feeds);
+};
+
+/**
+ * @param {Array<Object>} data
+ * @param {Object} htmlElements
+ * @param {Set} seenPosts
+ * @param {string} btnText
+ */
+const handlePosts = (data, htmlElements, seenPosts, btnText) => {
+  const { postsList } = htmlElements;
+  postsList.innerHTML = '';
+  const posts = [...data].map(({
+    title, id, link,
+  }) => {
+    const liElement = document.createElement('li');
+    liElement.classList
+      .add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    const postLink = document.createElement('a');
+    postLink.setAttribute('href', link);
+    postLink.setAttribute('target', '_blank');
+
+    if (seenPosts.has(id)) {
+      postLink.classList.add('fw-normal', 'text-dark');
+    } else {
+      postLink.classList.add('fw-bold');
+    }
+
+    postLink.textContent = title;
+    const postButton = document.createElement('button');
+    postButton.dataset.postId = id;
+    postButton.dataset.bsToggle = 'modal';
+    postButton.dataset.bsTarget = '#modal';
+    postButton.classList.add('btn', 'btn-primary');
+    postButton.textContent = btnText;
+    liElement.append(postLink, postButton);
+    return liElement;
+  });
+  postsList.append(...posts);
+};
+
+/**
+ * @param {string} postId
+ * @param {Object} htmlElements
+ */
+const markPostAsRead = (postId, htmlElements) => {
+  const { postsList } = htmlElements;
+  const selector = `button[data-post-id="${postId}"]`;
+  const linkElement = postsList.querySelector(selector).previousElementSibling;
+  linkElement.classList.remove('fw-bold');
+  linkElement.classList.add('fw-normal', 'text-dark');
+};
+
+/**
+ * @param {Object} clickedPost
+ * @param {Object} htmlElements
+ */
+const updateModalContents = (clickedPost, htmlElements) => {
+  const {
+    title, description, link,
+  } = clickedPost;
+  const {
+    modalHeading, modalBody, btnReadMore,
+  } = htmlElements;
+  modalHeading.textContent = title;
+  modalBody.textContent = description;
+  btnReadMore.setAttribute('href', link);
+};
+
+/**
+ * @param {boolean} isValid
+ * @param {Object} htmlElements
+ */
+
+const showFormValidationStatus = (isValid, htmlElements) => {
+  const { inputElement, feedbackField } = htmlElements;
+  inputElement.classList.toggle('is-invalid');
+  if (!isValid) {
+    feedbackField.classList.remove('text-success');
+    feedbackField.classList.add('text-danger');
+  }
+};
+
+/**
+ * @param {string} formState
+ * @param {Object} htmlElements
+ */
+
+const handleFormStateChange = (formState, htmlElements) => {
+  const {
+    btnSubmit, feedbackField, form, inputElement,
+  } = htmlElements;
+  switch (formState) {
+    case 'sending':
+      btnSubmit.setAttribute('disabled', 'disabled');
+      break;
+    case 'processed':
+      btnSubmit.removeAttribute('disabled');
+      feedbackField.classList.remove('text-danger');
+      feedbackField.classList.add('text-success');
+      form.reset();
+      inputElement.focus();
+      break;
+    case 'failed':
+      btnSubmit.removeAttribute('disabled');
+      feedbackField.classList.remove('text-success');
+      feedbackField.classList.add('text-danger');
+      break;
+
+    default:
+      break;
+  }
+};
+
+/**
+ * @param {string} message
+ * @param {Object} htmlElements
+ */
+const showFeedbackMessage = (message, htmlElements) => {
+  const { feedbackField } = htmlElements;
+  feedbackField.textContent = message;
+};
+
+/**
  *
  * @param {string} path
  * @param {*} value
@@ -8,118 +151,34 @@
  * @param {Object} state
  */
 const render = (path, value, prevValue, i18next, elements, state) => {
-  const {
-    form,
-    inputElement,
-    btnSubmit,
-    feedbackField,
-    rssContainer,
-    feedsList,
-    postsList,
-    modalHeading,
-    modalBody,
-    btnReadMore,
-  } = elements;
-
   if (path === 'feeds') {
-    rssContainer.classList.remove('d-none');
-    feedsList.innerHTML = '';
-    const feeds = [...value].map((feed) => {
-      const liElement = document.createElement('li');
-      liElement.classList.add('list-group-item', 'border-0');
-      const feedHeading = document.createElement('h3');
-      feedHeading.classList.add('h5', 'text-primary');
-      feedHeading.textContent = feed.title;
-      const feedDescription = document.createElement('p');
-      feedDescription.classList.add('text-small');
-      feedDescription.textContent = feed.description;
-      liElement.append(feedHeading, feedDescription);
-      return liElement;
-    });
-    feedsList.append(...feeds);
+    handleFeeds(value, elements);
   }
 
   if (path === 'posts') {
     const { ui: { seenPosts } } = state;
-    postsList.innerHTML = '';
-    const posts = [...value].map(({
-      title, id, link,
-    }) => {
-      const liElement = document.createElement('li');
-      liElement.classList
-        .add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
-      const postLink = document.createElement('a');
-      postLink.setAttribute('href', link);
-      postLink.setAttribute('target', '_blank');
-
-      if (seenPosts.has(id)) {
-        postLink.classList.add('fw-normal', 'text-dark');
-      } else {
-        postLink.classList.add('fw-bold');
-      }
-
-      postLink.textContent = title;
-      const postButton = document.createElement('button');
-      postButton.dataset.postId = id;
-      postButton.dataset.bsToggle = 'modal';
-      postButton.dataset.bsTarget = '#modal';
-      postButton.classList.add('btn', 'btn-primary');
-      postButton.textContent = i18next.t('ui.btnRead');
-      liElement.append(postLink, postButton);
-      return liElement;
-    });
-    postsList.append(...posts);
+    const localizedBtnText = i18next.t('ui.btnRead');
+    handlePosts(value, elements, seenPosts, localizedBtnText);
   }
 
   if (path === 'currentPostId' && value !== prevValue) {
     const { posts } = state;
-    const selector = `button[data-post-id="${value}"]`;
-    const linkElement = postsList.querySelector(selector).previousElementSibling;
-    linkElement.classList.remove('fw-bold');
-    linkElement.classList.add('fw-normal', 'text-dark');
-
     const currentPost = posts.find(({ id }) => id === value);
-    const {
-      title, description, link,
-    } = currentPost;
-    modalHeading.textContent = title;
-    modalBody.textContent = description;
-    btnReadMore.setAttribute('href', link);
+    markPostAsRead(value, elements);
+    updateModalContents(currentPost, elements);
   }
 
   if (path === 'rssForm.valid') {
-    inputElement.classList.toggle('is-invalid');
-    if (!value) {
-      feedbackField.classList.remove('text-success');
-      feedbackField.classList.add('text-danger');
-    }
+    showFormValidationStatus(value, elements);
   }
 
   if (path === 'rssForm.processingState') {
-    switch (value) {
-      case 'sending':
-        btnSubmit.setAttribute('disabled', 'disabled');
-        break;
-      case 'processed':
-        btnSubmit.removeAttribute('disabled');
-        feedbackField.classList.remove('text-danger');
-        feedbackField.classList.add('text-success');
-        form.reset();
-        inputElement.focus();
-        break;
-      case 'failed':
-        btnSubmit.removeAttribute('disabled');
-        feedbackField.classList.remove('text-success');
-        feedbackField.classList.add('text-danger');
-        break;
-
-      default:
-        break;
-    }
+    handleFormStateChange(value, elements);
   }
 
   if (path === 'rssForm.feedback' && value !== prevValue) {
-    feedbackField.textContent = i18next.t(value);
+    const localizedMessage = i18next.t(value);
+    showFeedbackMessage(localizedMessage, elements);
   }
 };
 
